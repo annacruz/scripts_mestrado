0053bc51bf5de769e8fe8e8f92e6c1b467a64acc
Anna Cruz <anna.cruz@gmail.com>
2013-07-31 21:43:40 -0300

    adding python to facilitate remote connections

---
+++ rpf/getting_remote_info.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import paramiko
+
+def paramikoConnect(host, username, password, timeout, port=22):
+    """Connects to 'host' and returns a Paramiko transport object to use in further communications"""
+    # Uncomment this line to turn on Paramiko debugging
+    #paramiko.util.log_to_file('paramiko.log')
+    ssh = paramiko.SSHClient()
+    try:
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(host, port=port, username=username, password=password, timeout=timeout)
+    except Exception, detail:
+        # Connecting failed (for whatever reason)
+        ssh = str(detail)
+    return ssh
+
+def sudoExecute(transport, command, password):
+    """Executes the given command via sudo.
+    Returns stdout, stderr (after command execution)"""
+    stdin, stdout, stderr = transport.exec_command("echo %s | sudo -S %s" % (password, command))
+    return stdout, stderr
+
+def format_output(output):
+    result = []
+    for line in output.readlines():
+        result.append(line.split('\n'))
+    return result
