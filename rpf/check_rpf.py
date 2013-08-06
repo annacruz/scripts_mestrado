00e0a575fd7337a9f4bba074e64ef1c51fa8b88c
Anna Cruz <anna.cruz@gmail.com>
2013-08-06 05:52:52 -0300

    Adding TODO

---
+++ rpf/check_rpf.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+## TODO:
+##  1 - Connect through ssh in a server/router
+##  2 - Get dict converted 'show ip bgp' from this router
+##  3 - Save this in mongodb collection
+##  4 - Choose an IP and search all possible networks in collection above
+##  5 - Get the interface of returned ip's (through 'show ip route xxx.xxx.xxx.xxx')
+
