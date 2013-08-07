a084217399f25a4539ac77fc96bb1c44e68b146f
Anna Cruz <anna.cruz@gmail.com>
2013-08-06 21:33:26 -0300

    wip (Check RPF) - Renaming for more apropriate name. Dividing into 2 scripts Check RPF

--- rpf/bgp_info.py
+++ rpf/bgp_info.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python
 
-## TODO:
-##  1 - Connect through ssh in a server/router
-##  2 - Get dict converted 'show ip bgp' from this router
-##  3 - Save this in mongodb collection
-##  4 - Choose an IP and search all possible networks in collection above
-##  5 - Get the interface of returned ip's (through 'show ip route xxx.xxx.xxx.xxx')
-
 from ssh_connection_util import Ssh
+from bgp_table_util import Util
 
 host = '192.169.1.101'
 username = 'ubuntu'
@@ -17,7 +11,11 @@ timeout = 60
 command = "sudo vtysh -c 'show ip bgp'"
 
 ssh = Ssh()
+util = Util()
+
 connection = ssh.connect(host=host, username=username, password=password, timeout=timeout)
 resultSet, stderr = ssh.sudoExecute(connection, command, password)
 
-print resultSet
+bgpDict = util.convert_to_dict(resultSet)
+
+print bgpDict
