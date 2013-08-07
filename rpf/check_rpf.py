c6dfbc434f27fe804f7a0843c34c64e32dd8aa68
Anna Cruz <anna.cruz@gmail.com>
2013-08-07 01:18:57 -0300

    (Check RPF) Effective Check RPF

--- rpf/check_rpf.py
+++ rpf/check_rpf.py
@@ -1,9 +1,21 @@
 #! /usr/bin/env python
+from pymongo import MongoClient
+import re
+import ipaddress
+
+client = MongoClient('localhost', 27017)
+db = client.stats
+collection = db.bgp_info
 
 entrance = '172.31.4.110'
 
 first_digits = entrance.split('.')[0]
+regex = re.compile(first_digits, re.IGNORECASE)
 
+resultSet = []
+for result in collection.find({"Network": regex }) : 
+  resultSet.append(result)
 
-
-print first_digits
+address = ipaddress.ip_address(u'%s'%entrance)
+for result in resultSet:
+  print address in ipaddress.ip_network(u'%s'%result['Network'])
