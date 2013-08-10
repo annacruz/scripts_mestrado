2702a6727d6b83c392b55cc9889a796022dd9b2a
Anna Cruz <anna.cruz@gmail.com>
2013-08-10 09:33:02 -0300

    Starting getting arguments from command line

--- rpf/check_rpf.py
+++ rpf/check_rpf.py
@@ -1,24 +1,28 @@
 #! /usr/bin/env python
 from pymongo import MongoClient
 import re
+import sys
 import ipaddress
 
 # TODO:
 #   Convert to a script with arguments and config file
+def execute(entrance):
+  client = MongoClient('localhost', 27017)
+  db = client.stats
+  collection = db.bgp_info
 
-client = MongoClient('localhost', 27017)
-db = client.stats
-collection = db.bgp_info
+  # entrance = '172.31.4.110'
 
-entrance = '172.31.4.110'
+  first_digits = entrance.split('.')[0]
+  regex = re.compile(first_digits, re.IGNORECASE)
 
-first_digits = entrance.split('.')[0]
-regex = re.compile(first_digits, re.IGNORECASE)
+  resultSet = []
+  for result in collection.find({"Network": regex }) :
+    resultSet.append(result)
 
-resultSet = []
-for result in collection.find({"Network": regex }) :
-  resultSet.append(result)
+  address = ipaddress.ip_address(u'%s'%entrance)
+  for result in resultSet:
+    print address in ipaddress.ip_network(u'%s'%result['Network'])
 
-address = ipaddress.ip_address(u'%s'%entrance)
-for result in resultSet:
-  print address in ipaddress.ip_network(u'%s'%result['Network'])
+if __name__ == '__main__':
+  execute(sys.argv[1])
