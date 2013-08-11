eda4e36bf8b54a94f5231663dc03cd3128be59a5
Anna Cruz <anna.cruz@gmail.com>
2013-08-11 19:14:08 -0300

    Adjusting some daemon things

--- rpf/bgp_info.py
+++ rpf/bgp_info.py
@@ -7,7 +7,7 @@ from bgp_table_util import Util
 from pymongo import MongoClient
 import json
 
-class BgpInfo:
+class BgpInfo(Daemon):
   global config
   config = json.load(open("config/config.cfg"))
 
@@ -41,18 +41,18 @@ class BgpInfo:
       collection.insert(bgpDict)
 
 if __name__ == "__main__":
-daemon = BgpInfo('/tmp/bgpinfo.pid')
-if len(sys.argv) == 2:
-  if 'start' == sys.argv[1]:
-    daemon.start()
-  elif 'stop' == sys.argv[1]:
-    daemon.stop()
-  elif 'restart' == sys.argv[1]:
-    daemon.restart()
+  daemon = BgpInfo('/tmp/bgpinfo.pid')
+  if len(sys.argv) == 2:
+    if 'start' == sys.argv[1]:
+      daemon.start()
+    elif 'stop' == sys.argv[1]:
+      daemon.stop()
+    elif 'restart' == sys.argv[1]:
+      daemon.restart()
+    else:
+      print "Unknown command"
+      sys.exit(2)
+    sys.exit(0)
   else:
-    print "Unknown command"
+    print "usage: %s start|stop|restart" % sys.argv[0]
     sys.exit(2)
-  sys.exit(0)
-else:
-  print "usage: %s start|stop|restart" % sys.argv[0]
-  sys.exit(2)
